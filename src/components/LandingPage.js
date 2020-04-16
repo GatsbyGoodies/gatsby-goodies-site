4996e6453f32b4ab48de57ff4ae1c9dc13e2aae1
Ryan Warner <warner.ryan@gmail.com>
2020-04-16 10:18:59 -0400

    Use infomred to handle form logic and state

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,58 +1,57 @@
-import React, { useState } from "react"
-import Amplify, { API } from "aws-amplify"
+import React, { useState, useRef } from 'react'
+import Amplify, { API } from 'aws-amplify'
+import { Form } from 'informed'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import awsconfig from "../aws-exports"
-import feedbackMap from "../data/feedbackMap"
+import Layout from '../components/layout'
+import TextField from '../components/TextField'
+import SEO from '../components/seo'
+import awsconfig from '../aws-exports'
+import feedbackMap from '../data/feedbackMap'
+import emailField from '../data/emailField'
 
 Amplify.configure(awsconfig)
 
 const IndexPage = () => {
   const [loading, setLoading] = useState(false)
-  const [feedback, setFeedback] = useState("DEFAULT")
-  const [email, setEmail] = useState("")
-  const handleChange = e => setEmail(e.target.value)
-  const subscribe = e => {
-    e.preventDefault()
-    setLoading(true)
-    API.post("api", "/subscribe", {
-      body: { email },
-    })
-      .then(res => {
-        setLoading(false)
-        setFeedback(res.type)
-      })
-      .catch(err => {
-        setLoading(false)
-        setFeedback(err.type)
-      })
+  const [feedback, setFeedback] = useState('DEFAULT')
+  const formApi = useRef()
+
+  const subscribe = async () => {
+    const formState = formApi.current.getState()
+    const { email } = formState.values
+
+    const data = { body: { email } }
+
+    try {
+      setLoading(true)
+      const res = await API.post('api', '/subscribe', data)
+      setLoading(false)
+      setFeedback(res.type)
+    } catch (error) {
+      setLoading(false)
+      setFeedback(error.type)
+    }
   }
+
   return (
     <Layout>
-      <SEO title="Home" />
-      <div className="logo">gatsby goodies</div>
-      <div className="headline">
+      <SEO title='Home' />
+      <div className='logo'>gatsby goodies</div>
+      <div className='headline'>
         Premium Gatsby themes to kickstart your blog, portfolio, or online
         store.
       </div>
-      <div className="signup">
+      <div className='signup'>
         <div className={feedbackMap[feedback].className}>
           {feedbackMap[feedback].message}
         </div>
-        <form className="form">
-          <input
-            onChange={handleChange}
-            value={email}
-            type="email"
-            name="email"
-            placeholder="you@email.com"
-          />
-          <div className="button" role="button" onClick={subscribe}>
+        <Form className='form' onSubmit={subscribe} apiRef={formApi}>
+          <TextField {...emailField} />
+          <button className='button'>
             <span style={{ opacity: loading ? 0 : 1 }}>Get Notified</span>
-            <div style={{ opacity: loading ? 1 : 0 }} className="loader"></div>
-          </div>
-        </form>
+            <div style={{ opacity: loading ? 1 : 0 }} className='loader'></div>
+          </button>
+        </Form>
       </div>
     </Layout>
   )
---
+++ src/components/TextField.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { asField, BasicText } from 'informed'
+
+export default asField((props) => {
+  const { label, noError, disabled, ...rest } = props
+  const error = props.fieldState.error
+
+  return (
+    <div>
+      {label && <label>{label}</label>}
+      <BasicText disabled={disabled} {...rest} />
+      {!noError && error && <div>{error}</div>}
+    </div>
+  )
+})
